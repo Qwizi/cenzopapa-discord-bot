88ed90334a318c31f877e8645d5e6bb5cc3565e9
Qwizi <ciolek.adrian@protonmail.com>
2021-11-29 19:16:24 +0100

    Updated bot

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,12 @@
-FROM gorialis/discord.py
+FROM python:3.9-alpine
 
 WORKDIR /app
 
+RUN apk add --no-cache --virtual .pynacl_deps build-base python3-dev libffi-dev
+
 COPY ./requirements.txt /app/requirements.txt
 
-RUN apt-get install ca-certificates
-RUN update-ca-certificates
+RUN pip install https://github.com/Qwizi/cenzopapa-sdk/archive/refs/tags/v1.0.2.tar.gz
 RUN pip install -r /app/requirements.txt \
     && rm -rf /root/.cache/pip
 COPY . /app/
--- bot.py
+++ bot.py
@@ -1,13 +1,17 @@
 import asyncio
 import os
-# import httpx
 from datetime import datetime
 
-from httpx import AsyncClient
+import pytz
+
+from cenzopapa import Cenzopapa
+from cenzopapa.schemas import Image
+
 from discord import Embed
-from discord.ext import commands, tasks
+from discord.ext import commands
 from dotenv import load_dotenv
-from schemas import Image
+
+tz = pytz.timezone('Europe/Warsaw')
 
 load_dotenv()
 
@@ -16,24 +20,20 @@ API_URL = os.getenv('API_URL')
 
 bot = commands.Bot(command_prefix="?")
 
+api = Cenzopapa(API_URL)
+
 
 def send_embed_image(image_url) -> Embed:
-    e = Embed(title="https://jebzpapy.tk")
+    e = Embed(title="https://jebzpapy.netlify.com")
     e.set_image(url=image_url)
     return e
 
 
 async def get_random_image() -> Image or None:
     try:
-        async with AsyncClient() as client:
-            response = await client.get(f"{API_URL}/aimages/random/")
-            if response.status_code == 200:
-                data = response.json()
-                print(data)
-                if data and data['id'] and data['public_url']:
-                    image = Image(id=data['id'], public_url=data['public_url'])
-                    return image
-            return None
+        image, _ = await api.image.random()
+        print(image)
+        return image
     except Exception as e:
         print(e)
         return None
@@ -52,37 +52,44 @@ async def get_10_random_images() -> list[Image] or []:
 async def get_default_channel(guild):
     return next((x for x in guild.text_channels if x.position == 0 or x.position == 1), None)
 
+async def send_10_random_images(guilds):
+    print('Rozpoczynamy godzine papierzowa')
+    for guild in guilds:
+        default_channel = await get_default_channel(guild)
+        channel = bot.get_channel(default_channel.id)
+        random_images = await get_10_random_images()
+        for image in random_images:
+            embed = send_embed_image(image.public_url)
+            await channel.send(embed=embed)
+
 @bot.event
 async def on_ready():
     print("Online")
+    print(await get_random_image())
     for guild in bot.guilds:
         channel = await get_default_channel(guild)
         print(channel.id)
 
+
 @bot.command()
 async def cenzo(ctx):
     image = await get_random_image()
-    embed = send_embed_image(image.public_url)
+    embed = send_embed_image(image.url)
     await ctx.send(embed=embed)
+
 
 async def send_random_10_images():
     await bot.wait_until_ready()
     while not bot.is_closed():
 
-        hour = int(datetime.now().time().strftime("%H"))
-        minutes = int(datetime.now().time().strftime("%M"))
+        hour = int(datetime.now(tz).time().strftime("%H"))
+        minutes = int(datetime.now(tz).time().strftime("%M"))
         print(f"{hour}:{minutes}")
         if hour == 21 and minutes == 37:
-            print('Rozpoczynamy godzine papierzowa')
-            for guild in bot.guilds:
-                default_channel = await get_default_channel(guild)
-                channel = bot.get_channel(default_channel.id)
-                random_images = await get_10_random_images()
-                for image in random_images:
-                    embed = send_embed_image(image.public_url)
-                    await channel.send(embed=embed)
+            await send_10_random_images(bot.guilds)
 
         await asyncio.sleep(60)
+
 
 bot.loop.create_task(send_random_10_images())
 bot.run(TOKEN)
