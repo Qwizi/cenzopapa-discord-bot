9fa5462987b0bb3f29e36aa3507950e638a5ba94
Qwizi <ciolek.adrian@protonmail.com>
2021-12-05 19:24:12 +0100

    HOTFIX

--- bot/instances.py
+++ bot/instances.py
@@ -1,5 +1,5 @@
 import os
-
+import discord
 import pytz
 from cenzopapa import Cenzopapa
 from discord.ext import commands
@@ -10,6 +10,14 @@ tz = pytz.timezone('Europe/Warsaw')
 
 TOKEN = os.getenv('TOKEN')
 API_URL = os.getenv('API_URL')
+LOCAL = int(os.getenv('LOCAL'), 0)
+PAPA_HOUR = int(os.getenv("PAPA_HOUR", 21))
+PAPA_MIN = int(os.getenv("PAPA_MIN", 37))
+
 API_ERROR = "Wystąpil problem z API"
+
 api = Cenzopapa(API_URL)
-bot = commands.Bot(command_prefix="?")
+bot = commands.Bot(command_prefix="??" if LOCAL else "?")
+watching_kids = discord.Activity(name='Dzieci', type=discord.ActivityType.watching)
+prepare_to_papa_hour = discord.Game(name='21:37')
+die = discord.Game(name='Nie zyje')
--- schemas.py
+++
@@ -1,7 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class Image:
-    id: int
-    public_url: str
